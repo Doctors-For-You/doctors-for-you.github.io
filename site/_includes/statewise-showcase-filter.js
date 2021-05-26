afeca0fa850e82308243fea6c81ca7e2c2c0e969
Saptarshi Biswas <saptarshi.17@gmail.com>
2021-05-25 18:14:33 -0700

    statewise showcase done

--- site/_includes/statewise-showcase-filter.js
+++ site/_includes/statewise-showcase-filter.js
@@ -1,24 +1,35 @@
 
 $(function() {
-  $('#partnerTab a').on('click', function (event) {
-    event.preventDefault();
-    var current_filter = $(this).attr('data-filter');
-    $('.logo_content').each(function(i, obj) {
-      var my_filter = $(obj).attr('data-filter');
-      if (current_filter === my_filter || current_filter === "all") {
-        $(obj).addClass('d-block').removeClass('d-none');
-      } else {
-        $(obj).addClass('d-none').removeClass('d-block');
-      }
-    });
-    
-    $('#partnerTab .nav-link').each(function(i, obj) {
-      var my_filter = $(obj).attr('data-filter');
-      if (current_filter === my_filter) {
-        $(obj).addClass('active');
-      } else {
-        $(obj).removeClass('active');
-      }
-    });
+  // $('#partnerTab a').on('click', function (event) {
+  //   event.preventDefault();
+  //   var current_filter = $(this).attr('data-filter');
+  //   $('.logo_content').each(function(i, obj) {
+  //     var my_filter = $(obj).attr('data-filter');
+  //     if (current_filter === my_filter || current_filter === "all") {
+  //       $(obj).addClass('d-block').removeClass('d-none');
+  //     } else {
+  //       $(obj).addClass('d-none').removeClass('d-block');
+  //     }
+  //   });
+  //
+  //   $('#partnerTab .nav-link').each(function(i, obj) {
+  //     var my_filter = $(obj).attr('data-filter');
+  //     if (current_filter === my_filter) {
+  //       $(obj).addClass('active');
+  //     } else {
+  //       $(obj).removeClass('active');
+  //     }
+  //   });
+  // });
+
+  $('#showcaseCarousel').on('slide.bs.carousel', function (ev) {
+    $('#statewiseTab .nav-link').removeClass('active');
+    $('#statewiseTab .nav-link').eq(ev.to).addClass('active');
+  });
+
+  $('#statewiseTab .nav-link').on('click', function(ev) {
+    ev.preventDefault();
+    var clickIndex = $(this).attr('data-index') - 1;
+    $('#showcaseCarousel').carousel(clickIndex);
   });
 });
